bee6f257668c951994661d7610530774f8a06a87
Crocobob911 <mjdaniel911@naver.com>
2025-09-03 21:35:21 +0900

    Refactors obstacle interaction logic
    Replaces scriptable object actions with data containers.
    
    This allows for more flexible obstacle behavior configuration,
    including score increases and ball property modifications.
    Removes unused CSV data and action scripts.

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -1,27 +1,43 @@
 using System.Collections.Generic;
+using Script;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
     [SerializeField]
-    private List<ObstacleAction> actions = new List<ObstacleAction>();
+    private List<ObstacleData> obstacleDatas = new List<ObstacleData>();
 
     protected virtual void OnCollisionEnter2D(Collision2D col)
     {
         if (!col.gameObject.CompareTag($"Ball")) return;
-        ExecuteActions();
-        
-        // TODO : Destroy가 아닌 pool을 사용할 것
-        Destroy(gameObject);
+        ExecuteAction();
     }
 
-    private void ExecuteActions()
+    private void ExecuteAction()
     {
-        foreach (var action in actions)
+        foreach (var obstacleData in obstacleDatas)
         {
-            if (action != null)
+            if (obstacleData == null) continue;
+
+            // 점수 Up
+            BallHitCountManager.AddBallHitCount( obstacleData.scoreIncrease );
+            
+            Debug.Log(obstacleData.ballSpeedChange +" "+ obstacleData.ballSizeChange +" "+ obstacleData.ballCountChange);
+            
+            // 공 속도 변화
+            // Debug.Log(obstacleData.ballSpeedChange);
+            
+            // 공 크기 변화
+            // Debug.Log(obstacleData.ballSizeChange);
+            
+            // 공 개수 증가
+            // Debug.Log(obstacleData.ballCountChange);
+            
+            // 오브젝트 삭제
+            if (obstacleData.isDisappear)
             {
-                action.Execute(gameObject);
+                // TODO : Destroy가 아닌 pool을 사용할 것
+                Destroy(gameObject);
             }
         }
     }
