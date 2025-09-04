638979545902286bde2906fbdf06e0bcfaf72121
Crocobob <mjdaniel911@naver.com>
2025-09-04 16:49:26 +0900

    Adds obstacle prefabs and ball controller
    Adds new obstacle prefabs with different effects.
    
    Also add BallController script to control the ball's speed on collision with any objects.
    
    Removes the old scene file.

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script {
+    public class BallController : MonoBehaviour {
+        [SerializeField]
+        private Rigidbody2D rb;
+        
+        [SerializeField]
+        private float speedOnCollision;
+
+        private void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (!col.gameObject.CompareTag($"Obstacle")) {
+                ApplyCollisionSpeedOnCollision();
+            }
+        }
+
+        private void ApplyCollisionSpeedOnCollision() {
+            Debug.Log("[ApplyCollisionSpeedOnCollision] 진입");
+            
+            Vector3 currentVelocity = rb.linearVelocity;
+            Vector3 direction = currentVelocity.normalized;
+            Vector3 newVelocity = direction * speedOnCollision;
+            
+            rb.linearVelocity = newVelocity;
+        }
+        
+        public void SetCollisionSpeed( float speed ) {
+            speedOnCollision = speed;
+        }
+    }
+}
